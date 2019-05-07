75fc88f24c783a9a31ce72a3ea6518fa797f911d
huanStephen <huanStephen@163.com>
2019-05-07 13:54:10 +0800

    控制层异常捕获

--- src/main/java/org/eocencle/dasislcy/exception/ControllerAspect.java
+++ src/main/java/org/eocencle/dasislcy/exception/ControllerAspect.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * @Auther: shizh26250
+ * 控制层异常捕获
+ * @Auther: huanStephen
  * @Date: 2018/12/3 20:25
  * @Description:
  */
@@ -17,6 +18,7 @@ public class ControllerAspect {
     @ExceptionHandler()
     public Result<?> testException(Exception e) {
         Result<?> result = new Result<>(Result.STATUS_FAILED);
+        e.printStackTrace();
         result.setMsg("系统异常！");
         return result;
     }
