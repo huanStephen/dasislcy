dff944977fd5eca5735192dd815af61abb1bd16c
huanStephen <huanStephen@163.com>
2019-08-20 22:04:41 +0800

    试卷内容打印开发

---
+++ src/main/resources/static/assets/js/page/exampaper_list.js
@@ -0,0 +1,65 @@
+(function() {
+    var model = new Model({
+        el: 'body',
+        init: {
+            open: true
+        },
+        remote: {
+            initLoad: {
+                path: 'web/exampaper/getExamPapers',
+                params: {
+                    currPage: 1,
+                    pageSize: 10
+                }
+            }
+        },
+
+        include: {
+            initRenderFilter: function(fieldName, value, $el, idx, obj) {
+                if (-1 != fieldName.indexOf('id')) {
+                    return $el.attr('href') + value;
+                }
+                return value;
+            },
+
+            initRenderAttr: function(fieldName) {
+                if ('id' == fieldName) {
+                    return 'href';
+                }
+            },
+
+            initLoadAfter: function(data) {
+                if (!this.isLoadPage) {
+                    this.page = new Paging;
+                    this.page.init({
+                        target: '#page',
+                        current: data.currPage,
+                        pagesize: 3,
+                        count: data.total,
+                        callback: this.proxy(function(page, size, count) {
+                            this.comp('remote', ['initLoad', function(config) {
+                                config.params.currPage = page;
+                            }]);
+                        })
+                    });
+                    this.isLoadPage = true;
+                } else {
+                    this.page.render({
+                        current: data.currPage,
+                        count: data.total,
+                        callback: this.proxy(function(page, size, count) {
+                            this.comp('remote', ['initLoad', function(config) {
+                                config.params.currPage = page;
+                            }]);
+                        })
+                    });
+                }
+                return data;
+            },
+
+            initRenderComplete: function(initData) {
+                $(".knob").knob();
+            }
+        }
+    });
+})();
