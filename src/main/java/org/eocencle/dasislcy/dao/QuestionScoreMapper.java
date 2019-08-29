103c3d4d857a0b94c73cd40284bb109cda01dca9
huanStephen <huanStephen@163.com>
2019-08-29 19:48:08 +0800

    试题分数实体

---
+++ src/main/java/org/eocencle/dasislcy/dao/QuestionScoreMapper.java
@@ -0,0 +1,7 @@
+package org.eocencle.dasislcy.dao;
+
+import org.eocencle.dasislcy.entity.QuestionScoreEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface QuestionScoreMapper extends Mapper<QuestionScoreEntity> {
+}
