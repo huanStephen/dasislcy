fbf5735ad90d790e20623be5e5008173c819febd
huanStephen <huanStephen@163.com>
2019-09-07 09:09:31 +0800

    添加学生考卷实例

---
+++ src/main/java/org/eocencle/dasislcy/dao/StudentExampaperMapper.java
@@ -0,0 +1,7 @@
+package org.eocencle.dasislcy.dao;
+
+import org.eocencle.dasislcy.entity.StudentExampaperEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface StudentExampaperMapper extends Mapper<StudentExampaperEntity> {
+}
