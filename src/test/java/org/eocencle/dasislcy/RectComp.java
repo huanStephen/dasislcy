264455afb8ebe223193b83c92c14e96ca5ae02fd
huanStephen <huanStephen@163.com>
2019-09-12 14:41:26 +0800

    0912更新

--- src/test/java/org/eocencle/dasislcy/RectComp.java
+++ src/test/java/org/eocencle/dasislcy/RectComp.java
@@ -2,34 +2,56 @@ package org.eocencle.dasislcy;
 
 import org.opencv.core.Rect;
 
-public class RectComp {
-	
-	Rect rm ;
+public class RectComp implements Comparable<Object> {
+	private Rect rm;
 
-	public RectComp(Rect rms) {
-		super();
-		this.rm = rms;
-	}
-	
-	
-	
-	
 	public Rect getRm() {
 		return rm;
 	}
-
-
-
 
 	public void setRm(Rect rm) {
 		this.rm = rm;
 	}
 
+	public RectComp() {
+		super();
+	}
 
+	public RectComp(Rect rm) {
+		super();
+		this.rm = rm;
+	}
 
-
-	public boolean operator(RectComp ti) {
-		 return rm.x < ti.rm.x;  
+	// @Override
+	// public int compareTo(Object object) {
+	// if(this == object){
+	// return 0;
+	// } else if (object != null && object instanceof RectComp) {
+	// RectComp rect = (RectComp) object;
+	// if (rm.x <= rect.rm.x) {
+	// return -1;
+	// }else{
+	// return 1;
+	// }
+	// }else{
+	// return -1;
+	// }
+	// }
+	@Override
+	// 按面积排序，最大的放第一个
+	public int compareTo(Object object) {
+		if(this == object){
+			return 0;
+		} else if (object != null && object instanceof RectComp) {
+			RectComp rect = (RectComp) object;
+			if (rm.area() >= rect.rm.area()) {
+				return -1;
+			} else {
+				return 1;
+			}
+		} else {
+			return -1;
+		}
 	}
 
 }
