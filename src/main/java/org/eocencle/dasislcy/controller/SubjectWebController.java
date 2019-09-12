5ea44bbe8974ed1d670dcf37e3e6112e1e17d817
jskdcc <765960789@qq.com>
2019-09-12 17:03:36 +0800

    修改上传下载页面样式

--- src/main/java/org/eocencle/dasislcy/controller/SubjectWebController.java
+++ src/main/java/org/eocencle/dasislcy/controller/SubjectWebController.java
@@ -27,6 +27,9 @@ public class SubjectWebController {
     @Value("${main.template-dir}")
     private String templateDir;
 
+    @Autowired
+    private ISubjectService subjectService;
+
     /**
      * 文件上传
      * @param file
@@ -37,15 +40,11 @@ public class SubjectWebController {
         Result<Boolean> result = new Result<>(Result.STATUS_SUCCESSED);
 
         try {
-            long  startTime=System.currentTimeMillis();
-            logger.debug("fileName："+file.getOriginalFilename());
             String path=uploadDir + "/" + new Date().getTime() + file.getOriginalFilename();
-
             File newFile=new File(path);
-            //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+            //通过CommonsMultipartFile的方法直接写文件
             file.transferTo(newFile);
-            long endTime=System.currentTimeMillis();
-            logger.debug("采用file.Transto的运行时间："+String.valueOf(endTime-startTime)+"ms");
+            subjectService.importSubjectFile(path);
 
             result.setData(true);
             result.setMsg("文件上传成功！");
@@ -68,31 +67,44 @@ public class SubjectWebController {
     @ResponseBody
     public String download(HttpServletResponse response) {
         String fileName = "template.xlsx";
+        //设置文件路径
         File file = new File(templateDir + "/" + fileName);
-        byte[] buff = new byte[1024];
-        BufferedInputStream bis = null;
-        OutputStream os = null;
-        try {
-            os = response.getOutputStream();
-            bis = new BufferedInputStream(new FileInputStream(file));
-            int i = bis.read(buff);
-            while (i != -1) {
-                os.write(buff, 0, buff.length);
-                os.flush();
-                i = bis.read(buff);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "文件下载失败";
-        } finally {
-            if (bis != null) {
-                try {
-                    bis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        //File file = new File(realPath , fileName);
+        if (file.exists()) {
+            response.setContentType("application/force-download");// 设置强制下载不打开
+            response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+                return "下载成功";
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
-        return "文件下载成功";
+        return "下载失败";
     }
 }
